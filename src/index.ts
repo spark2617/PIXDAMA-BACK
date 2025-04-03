e774b3284b57b0049efc48033ed861814a8263a9
jhonatan <jhonatandesouzade13@gmail.com>
2025-04-03 14:04:43 -0400

    ajuste 1

--- src/index.ts
+++ src/index.ts
@@ -23,14 +23,6 @@ app.use(cors({
     exposedHeaders: ['Set-Cookie']
 }));
 
-app.use((req: Request, res: Response, next) => {
-    res.header('Access-Control-Allow-Origin');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
 
 app.use(express.json());
 app.use(cookieParser());
