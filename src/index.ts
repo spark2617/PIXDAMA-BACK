5929128a93c11cf3e264f024cc5e1503c4bfaf59
jhonatan <jhonatandesouzade13@gmail.com>
2025-04-04 10:10:27 -0400

    ajuste cors origin 4

--- src/index.ts
+++ src/index.ts
@@ -19,8 +19,9 @@ app.use(cors({
     origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
-    exposedHeaders: ['Set-Cookie']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['Set-Cookie'],
+    
 }));
 
 
